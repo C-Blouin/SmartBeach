e20163be756189104aa71dd2462c9bc549b1f2af
Marina231 <marinagr3006@gmail.com>
2024-02-29 18:04:19 -0500

    contact-us page css styles mobile and desktop

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,28 +1,52 @@
-import React from "react";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
+
 function Contact() {
   useEffect(() => {
     document.title = "Smart Beach | Contact Us";
   }, []);
 
   return (
-    <div className="contact_us">
-      <section className="contact_us_header"></section>
+    <div className="contact-us">
+      <section className="contact-us-header row"></section>
       <div className="container">
-        <section className="contact_us_info">
-          <div className="contact_info">
+        <section className="contact-us-info row">
+          <div className="contact-info col-lg-6 col-md-10">
             <h1>Get in Touch</h1>
             <p>
               Feel free to reach out to us with any questions or concerns! Lorem
               ipsum dolor sit amit consectetur. Dapibus at urna neque.
             </p>
             <ul>
-              <li>smartbeach@gmail.com</li>
-              <li>(519)764-0000</li>
-              <li>200 Station Beach Road Kincardine, Ontario</li>
+              <li>
+                <img
+                  src="icons/email-icon.svg"
+                  width="35px"
+                  height="35px"
+                  alt="icon"
+                />
+                <p>smartbeach@gmail.com</p>
+              </li>
+              <li>
+                <img
+                  src="icons/phone-icon.svg"
+                  width="35px"
+                  height="35px"
+                  alt="icon"
+                />
+                <p>(519)764-0000</p>
+              </li>
+              <li>
+                <img
+                  src="icons/mail-icon.svg"
+                  width="35px"
+                  height="35px"
+                  alt="icon"
+                />
+                <p>200 Station Beach Road Kincardine, Ontario</p>
+              </li>
             </ul>
           </div>
-          <div className="contact_us_form">
+          <div className="contact-us-form col-lg-4 col-sm-4">
             <form action="#" method="post">
               <fieldset>
                 <label for="contact-name">Name:</label>
@@ -38,9 +62,10 @@ function Contact() {
                 <label for="contact-phone">Phone:</label>
                 <input
                   type="tel"
+                  pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
                   name="contact-phone"
                   id="contact-phone"
-                  placeholder="Contact Phone"
+                  placeholder="Contact Phone (123-456-7890)"
                   required
                 ></input>
               </fieldset>
