8dc06e6dde61b8fee1f216eccc6a59148e77f118
Marina231 <marinagr3006@gmail.com>
2024-03-05 14:01:22 -0500

    contact-us changes/dashboard navigation/dashboard safety indicators

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ import Footer from "./views/shared/Footer.jsx";
 
 // Imports for Page Views
 import Home from "./views/Home.jsx";
-import BeachInfo from './views/BeachInfo.jsx';
+import BeachInfo from "./views/BeachInfo.jsx";
+import Dashboard from "./views/Dashboard.jsx";
 import Contact from "./views/Contact.jsx";
 
 function App() {
@@ -23,7 +24,8 @@ function App() {
           <Routes>
             {/* Place Page Component Routes Here (e.g. Home, About, Dashboard, etc.) */}
             <Route path="/" element={<Home />} />
-            <Route path="/beach-info" element={<BeachInfo />} />   
+            <Route path="/beach-info" element={<BeachInfo />} />
+            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </BrowserRouter>
