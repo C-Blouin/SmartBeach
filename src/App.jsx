27dbdd0a9976afdb4e8e22da9329361b32526658
Marina231 <marinagr3006@gmail.com>
2024-03-11 16:07:55 -0400

    About the project page/ HTML&CSS

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Footer from "./views/shared/Footer.jsx";
 
 // Imports for Page Views
 import Home from "./views/Home.jsx";
+import About from "./views/About.jsx";
 import BeachInfo from "./views/BeachInfo.jsx";
 import Dashboard from "./views/Dashboard.jsx";
 import Contact from "./views/Contact.jsx";
@@ -24,6 +25,7 @@ function App() {
           <Routes>
             {/* Place Page Component Routes Here (e.g. Home, About, Dashboard, etc.) */}
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
             <Route path="/beach-info" element={<BeachInfo />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/contact" element={<Contact />} />
