2a69410a6a2a637d30194e1cb3aae8d8697c7f07
Jenna Deamer <jennadeamer@gmail.com>
2024-03-12 21:15:32 -0400

    fixed arrow svg colours

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -20,17 +20,17 @@ function Dashboard() {
                 <h1>
                   <span id="dashboard-temperature-label">24 º </span>
                 </h1>
-                <img src={sunVector} width="67" height="67" id="weather-icon" alt="sun-icon" />
+                <img src={sunVector} width="67" height="67" id="weather-icon" alt="sun-icon"/>
               </div>
               <p id="dashboard-weather-condition-label">Sunny</p>
               <div className="temperature-container">
                 <div>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-up" viewBox="0 0 16 16"><path fillRule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5" />
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#3499D7" className="bi bi-arrow-up" viewBox="0 0 16 16"><path fillRule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5" />
                   </svg>
                   <p>H: <span>32°</span></p>
                 </div>
                 <div>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-down" viewBox="0 0 16 16"><path fillRule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1" />
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#3499D7" className="bi bi-arrow-down" viewBox="0 0 16 16"><path fillRule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1" />
                   </svg>
                   <p>L: <span>16°</span></p>
                 </div>
