18a4e2592caa07dd2a12f053577b5b21bd7e8550
C-Blouin <CBlouin.Georgian@gmail.com>
2024-03-21 15:43:09 -0400

    OpenWeather API Call and Value Conversions

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,71 +2,114 @@
 import React from "react";
 import { useState, useEffect, useContext } from "react";
 
-// Hero Forecast Widget Icon Imports.
-import thunderstorm from "../images/landing/forecast-icons/thunderstorms.svg";
-import windSpeed from "../images/landing/forecast-icons/wind.svg";
-import rainChance from "../images/landing/forecast-icons/umbrella.svg";
+// Weather Icon Imports for Icon Object to dynamically render the weather icons based on the weather condition.
 
-// Indicators Imports.
+// 01d, 01n - Clear Sky
+import sunnyIcon from "../images/dashboard/weather/sun.svg";
+import sunnyIconNight from "../images/dashboard/weather/sun.svg";
+// 02d, 02n - Sun with Clouds
+import sunWithCloudsIcon from "../images/dashboard/weather/sun-light-clouds.svg";
+import sunWithCloudsIconNight from "../images/dashboard/weather/sun-light-clouds.svg";
+// 03d, 03n - Clouds
+import cloudIcon from "../images/dashboard/weather/cloud.svg";
+import cloudIconNight from "../images/dashboard/weather/cloud.svg";
+// 04d, 04n - Broken Clouds
+import partlyCloudyIcon from "../images/dashboard/weather/cloud.svg";
+import partlyCloudyIconNight from "../images/dashboard/weather/cloud.svg";
+// 09d, 09n - Clouds with Showers
+import cloudWithRainIcon from "../images/dashboard/weather/cloud-with-rain.svg";
+import cloudWithRainIconNight from "../images/dashboard/weather/cloud-with-rain.svg";
+// 10d, 10n - Sun, Cloud, and Rain
+import sunCloudsRainIcon from "../images/dashboard/weather/sun-clouds-with-rain.svg";
+import sunCloudsRainIconNight from "../images/dashboard/weather/sun-clouds-with-rain.svg";
+// 11d, 11n - Thunderstorm
+import cloudWithLightningIcon from "../images/dashboard/weather/cloud-with-lightning-and-rain.svg";
+import cloudWithLightningIconNight from "../images/dashboard/weather/cloud-with-lightning-and-rain.svg";
+// 13d, 13n - Snow
+import snowyIcon from "../images/dashboard/weather/cloud-with-snow.svg";
+import snowyIconNight from "../images/dashboard/weather/cloud-with-snow.svg";
+
+// Static Panel Icon Imports
+import windSpeedImage from "../images/landing/forecast-icons/wind.svg";
+import rainChanceImage from "../images/landing/forecast-icons/umbrella.svg";
+import humidityImage from "../images/dashboard/panels/humidity-index.svg";
+
+// Safety Indicator Imports
 import safeIndicator from "../images/landing/safety-indicators/safe.svg";
 import cautionIndicator from "../images/landing/safety-indicators/caution.svg";
 import dangerIndicator from "../images/landing/safety-indicators/danger.svg";
 
 // Why Section Icon Imports
 import reasonIcon from "../images/landing/reason-icons/reason-icon.svg";
-
 // About Image Imports
 import aboutImage from "../images/landing/about/about-image.svg";
-
 // Review Images Imports
 import reviewImage from "../images/landing/reviews/laura.png";
-
 // FAQS Image Imports
 import lifeBuoy from "../images/landing/faq/life-buoy.png";
 
 function Home() {
-  // Weather API
-  const [weatherData, setWeatherData] = useState([]);
-  const url =
-    "https://weather338.p.rapidapi.com/weather/forecast?date=20240319&latitude=44.17489200&longitude=-81.63615400&language=en-US&units=m";
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "9e50bcac30msh30b61ae579c247ap1caef3jsn9fbd44d87e03",
-      "X-RapidAPI-Host": "weather338.p.rapidapi.com",
-    },
-  };
 
-  // Setting Document Title for the Home Page.
+  document.title = "SmartBeach | Home";
+
+  // Setting the initial state of the weather data to null, which will be populated later with the fetched data from the API.
+  const [weatherData, setWeatherData] = useState(null);
+
+  // useEffect Hook to fetch the weather data from the API, and update the state with the fetched data.
   useEffect(() => {
-    document.title = "Smart Beach | Home";
-
-    ///////////////////////////////////////////////////////////
-    const fetchData = async () => {
+    const fetchWeatherData = async () => {
+      // Try to fetch the data, if there is an error, log the error to the console.
       try {
-        const response = await fetch(url, options);
+        const response = await fetch('https://api.openweathermap.org/data/2.5/forecast/daily?lat=44.18339&lon=-81.63307&cnt=7&appid=b50c1df1821232d52ebcfef4330bb7d6');
         if (!response.ok) {
-          console.log(response);
-          setErrorMessage(response.statusText);
+          console.log(`Error fetching weather data: ${response.status} ${response.statusText}`)
         }
-        const result = await response.json();
-        setWeatherData(result["v3-wx-observations-current"]);
-        console.log(result);
-      } catch (error) {
-        console.log(`error: ${error}`);
-        setErrorMessage(error);
+        // IF the response is successful, parse the response into JSON and set the weatherData state to the fetched data.
+        const data = await response.json();
+        console.log(data);
+        setWeatherData(data);
+      } 
+      // Catch any errors and log them to the console.
+      catch (error) {
+        console.error('Error fetching weather data:', error);
       }
     };
-    // fetchData();
+
+    // Invoking the fetchWeatherData function to fetch the weather data from the API.
+    fetchWeatherData();
   }, []);
 
-  let temperature;
+  /* 
+  
+  Convertion & Data Formatting Functions 
+  
+  */
 
-  if (weatherData != null && weatherData.temperature != null) {
-    temperature = weatherData.temperature;
-  } else {
-    temperature = "N/A";
-  }
+  // Kelvin to Celsius
+  const convertKelvinToCelsius = (kelvin) => {
+    return (kelvin - 273.15).toFixed(1);
+  };
+
+  // Capitalize the first letter of each word in a string
+  const capitalizeEachWord = (word) => {
+    return word.replace(/\b\w/g, (char) => char.toUpperCase());
+  };
+ 
+  // Each day the API is fetching a new 7 day forecast, so these calculations will be based on the first day or the current day of the forecast.
+  const temperature = weatherData && Math.round(convertKelvinToCelsius(weatherData.list[0].temp.day)) + "°";
+  // Min & Max Temperature for the current day
+  const minTemperature = weatherData && Math.round(convertKelvinToCelsius(weatherData.list[0].temp.min)) + "°";
+  const maxTemperature = weatherData && Math.round(convertKelvinToCelsius(weatherData.list[0].temp.max)) + "°";
+  // Weather Condition (Light Snow, Sunny, etc). Passing it through the capitalizeEachWord function to capitalize the first letter of each word for consistentcy.
+  const condition = weatherData && capitalizeEachWord(weatherData.list[0].weather[0].description);
+  // Wind Speed & Gusts, these values are in meters per second, but we want to display it in kilometers per hour, using the formula: (m/s * 3.6) = km/h, and fixing the value to to 2 decimal places.
+  const windSpeed = weatherData && Math.round((weatherData.list[0].speed * 3.6).toFixed(2));
+  // Wind Gusts, converting meters per second to kilometers per hour
+  const windGusts = weatherData && Math.round((weatherData.list[0].gust * 3.6).toFixed(2));
+  // Humidity
+  const humidity = weatherData && weatherData.list[0].humidity;
+  // Probability of precipitation, converting it to a percentage.
+  const rainChance = weatherData && (weatherData.list[0].pop * 100).toFixed(0);
 
   return (
     <div className="container-fluid p-0">
@@ -85,13 +128,15 @@ function Home() {
 
           {/* Start of Hero Forecast */}
           <div className="hero-forecast">
+
             {/* Primary Card */}
             <div className="primary-card">
-              <img src={thunderstorm} alt="Thunderstorm" />
+              <img src={weatherIcon} alt="Thunderstorm" />
               <div className="primary-card-content">
                 <article>
-                  <h3>{temperature}°</h3>
-                  <p>Thunderstorms</p>
+                  {/* Passing the temperature value into the convertKelvin to Celsius function to render the temperature in a more friendly manner, fixed to 1 decimal place. */}
+                  <h3>{temperature}</h3>
+                  <p>{condition}</p>
                 </article>
                 <div className="temperature-container">
                   <div>
@@ -108,7 +153,8 @@ function Home() {
                         d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5"
                       />
                     </svg>
-                    <p>34°</p>
+                    {/* Max Temperature */}
+                    <p>{maxTemperature}</p>
                   </div>
                   <div>
                     <svg
@@ -124,7 +170,7 @@ function Home() {
                         d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1"
                       />
                     </svg>
-                    <p>22°</p>
+                    <p>{minTemperature}</p>
                   </div>
                 </div>
               </div>
@@ -135,14 +181,14 @@ function Home() {
               <div className="widget-top">
                 <p>Wind Speed</p>
                 <img
-                  src={dangerIndicator}
-                  alt="Danger Indicator"
+                  src={windSpeedIndicatorSrc}
+                  alt={windSpeedIndicatorAlt}
                   className="safety-indicators"
                 />
               </div>
-              <img src={windSpeed} alt="Wind Speed Icon" />
+              <img src={windSpeedImage} alt="Wind Speed Icon" />
               <p>
-                103 <small>km/h</small>
+                {windSpeed} <small>km/h</small>
               </p>
             </article>
 
@@ -151,29 +197,29 @@ function Home() {
               <div className="widget-top">
                 <p>Wind Gust</p>
                 <img
-                  src={cautionIndicator}
-                  alt="Caution Indicator"
+                  src={windGustsIndicatorSrc}
+                  alt={windGustsIndicatorAlt}
                   className="safety-indicators"
                 />
               </div>
-              <img src={windSpeed} alt="Wind Gust Icon" />
+              <img src={windSpeedImage} alt="Wind Gust Icon" />
               <p>
-                54 <small>km/h</small>
+                {windGusts} <small>km/h</small>
               </p>
             </article>
 
             {/* Sub Card Three */}
             <article className="widget-card">
               <div className="widget-top">
-                <p>Rain Chance</p>
+                <p>Humidity</p>
                 <img
-                  src={safeIndicator}
-                  alt="Safe Indicator"
+                  src={humidityIndicatorSrc}
+                  alt={humidityIndicatorAlt}
                   className="safety-indicators"
                 />
               </div>
-              <img src={rainChance} alt="Rain Chance Icon" />
-              <p>20%</p>
+              <img src={humidityImage} alt="Rain Chance Icon" />
+              <p>{humidity}%</p>
             </article>
 
             {/* Sub Card Four */}
@@ -181,13 +227,13 @@ function Home() {
               <div className="widget-top">
                 <p>Rain Chance</p>
                 <img
-                  src={dangerIndicator}
-                  alt="Danger Indicator"
+                  src={rainChanceIndicatorSrc}
+                  alt={rainChanceIndicatorAlt}
                   className="safety-indicators"
                 />
               </div>
-              <img src={rainChance} alt="Rain Chance Icon" />
-              <p>80%</p>
+              <img src={rainChanceImage} alt="Rain Chance Icon" />
+              <p>{rainChance}%</p>
             </article>
 
             {/* Forecast Call to Action */}
@@ -294,7 +340,7 @@ function Home() {
               quae ab illo inventore veritatis et quasi architecto beatae vitae
               dicta sunt explicabo.
             </p>
-            <a href="#">Read More</a>
+            <a href="/about">Read More</a>
           </article>
         </div>
       </div>
